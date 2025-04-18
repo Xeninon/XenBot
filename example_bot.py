7f5aebf73902191a06005eb294ba295f2cc33611
Xeninon <alecdscout@gmail.com>
2025-04-17 22:10:15 -0500

    hello world

---
+++ example_bot.py
@@ -0,0 +1,29 @@
+import discord
+import logging
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+token = os.environ.get("TOKEN")
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello World!')
+    
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+
+client.run(token, log_handler=handler)
